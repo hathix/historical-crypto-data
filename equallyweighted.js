8beac335cd42f8a70171293b519340960970cd7d
Neel Mehta <neelmehta96@gmail.com>
2021-12-17 11:27:25 -0800

    typing

--- equallyweighted.js
+++ equallyweighted.js
@@ -11,12 +11,16 @@ const dirname = process.cwd(); // Doesn't include the trailing slash
 const parser = parse({columns: true}, function (err, records) {
   // This is an array of coin details (with id, symbol, name).
   // Once we get this, read the prices & market caps of each coin
-  // back on Jan 1 20
-  for (let i = 0; i < records.length; i++) {
-    setTimeout((i) => {
-      console.log(`Getting data for ${records[i].name}`);
-      getHistoricalDataFor(records[i].id);
-    }, 2000 * i, i);
-  }
+  // back on Jan 1, 2021
+  const innerParser = parse({columns: true}, function(innerErr, innerRecords) {
+    // This is the contents of one of the CSV files for each coin.
+    // Once we get this, look up the row where t=1609459200000
+    // (That's Jan 1, 2021)
+    
+  });
+  // Loop over each record
+  records.forEach(record => {
+    createReadStream(`${dirname}/coins/${record.id}.csv`).pipe(innerParser);
+  });
 });
 createReadStream(`${dirname}/coindata.csv`).pipe(parser);
