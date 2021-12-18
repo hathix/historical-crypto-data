017817dfeafb09d66bac337b572d8c7f310990c6
Neel Mehta <neelmehta96@gmail.com>
2021-12-18 09:45:11 -0800

    ad some lib files

---
+++ lib.js
@@ -0,0 +1,58 @@
+/**
+  Reusable function library.
+*/
+
+import { stringify } from 'csv-stringify';
+import { parse } from 'csv-parse';
+import { parse as parseSync } from 'csv-parse/sync';
+import { writeFile, createReadStream, createWriteStream, readFileSync } from 'fs';
+import _ from 'lodash';
+
+// For reading/writing files
+export const dirname = process.cwd(); // Doesn't include the trailing slash
+
+
+/**
+  Synchronously reads and returns the list of all coins we track.
+  Each record will include:
+
+  - id
+  - symbol
+  - name
+
+  All are strings.
+*/
+export function getCoinList() {
+  // Read our list of all the top coins
+  const coinFile = readFileSync(`${dirname}/coindata.csv`);
+  const coinList = parseSync(coinFile, {
+    columns: true,
+    cast: true,
+  });
+  return coinList;
+}
+
+/**
+  Synchronously gets the historical market data for a coin with the
+  given ID (use getCoinList() for a list of those).
+  The returned value is an array of dicts, each of which represents a certain
+  day. It includes:
+
+  - timestamp: number
+  - readableTimestamp: string
+  - coinId: string
+  - price: number
+  - marketCap: number
+  - totalVolume: number
+*/
+export function getHistoricalData(coinId) {
+  // Read the CSV for that coin
+  const coinHistoryFile = readFileSync(`${dirname}/coins/${coinId}.csv`);
+  const coinHistoryData = parseSync(coinHistoryFile, {
+    columns: true,
+    cast: true,
+  });
+  return coinHistoryData;
+}
+
+console.log(getHistoricalData("shiba-inu"));
