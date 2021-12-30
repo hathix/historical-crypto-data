d8b66c6a826746aef02b758f53e903b389c72b9b
Neel Mehta <neelmehta96@gmail.com>
2021-12-30 11:28:42 -0500

    can compute holdings

--- buyall.js
+++ buyall.js
@@ -1,6 +1,9 @@
 import { dirname, getCoinList, getHistoricalData, getDataForDay, dateToTimestamp, writeDictToCsv } from "./lib.js";
 import { calcMarketDataOn } from "./calcindex.js";
 
+import _ from "lodash";
+
+
 /**
   Returns data on the top N coins by market cap on the given timestamp.
 */
@@ -14,5 +17,23 @@ export function getTopNCoinsOn(n, timestamp) {
   return topNCoins;
 }
 
+/**
+  If you bought a certain number of dollars of the given coin, how many
+  would you own? This returns a new coinData object that adds a "holdings" field.
+*/
+export function computeHoldingsOf(coinData, dollarsToBuy) {
+  // Clone the object
+  const newCoinData = _.clone(coinData);
+  
+  // Add a new field that's simply the dollars divided by the price
+  newCoinData.numCoinsHeld = dollarsToBuy / newCoinData.price;
+  // Also report the original cost basis, just so we have it
+  newCoinData.costBasis = dollarsToBuy;
+
+  // OK we're good
+  return newCoinData;
+}
+
+
 // export function
-console.log(getTopNCoinsOn(10, 1639785600000));
+console.log(getTopNCoinsOn(10, 1639785600000).map(c => computeHoldingsOf(c, 1000)));
